7e9ee2f30793a54cb008966e7fbf840e1dd1c394
developerplus2025 <anpham08112009@gmail.com>
2025-06-22 13:46:01 +0700

    refactor: update transition easing functions in PageTransition component

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -4,14 +4,27 @@ import { motion } from "framer-motion";
 
 const transitionVariants = {
   initial: { top: "100%" },
-  animate: { top: "0%", translateY:"-100%", transition: { duration: 2, ease: "easeInOut" } },
-  exit: { y: "-100%", transition: { duration: 0.6, ease: "easeInOut" } },
+  animate: {
+    top: "0%",
+    translateY: "-100%",
+    transition: {
+      duration: 2,
+      ease: [0.42, 0, 0.58, 1] as [number, number, number, number], // cubic-bezier tương đương "easeInOut"
+    },
+  },
+  exit: {
+    y: "-100%",
+    transition: {
+      duration: 0.6,
+      ease: [0.42, 0, 0.58, 1] as [number, number, number, number],
+    },
+  },
 };
 
 export default function PageTransition() {
   return (
     <motion.div
-      className="fixed top-0 left-0 w-full h-full bg-black z-[9999]"
+      className="fixed left-0 top-0 z-[9999] h-full w-full bg-black"
       variants={transitionVariants}
       initial="initial"
       animate="animate"
