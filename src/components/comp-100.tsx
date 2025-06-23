197a41b8aacdcc65aa711f18503396e49e311791
developerplus2025 <anpham08112009@gmail.com>
2025-06-23 19:32:18 +0700

    fix: update SVG properties in NavigationIcon component for improved rendering

--- src/components/comp-100.tsx
+++ src/components/comp-100.tsx
@@ -17,15 +17,11 @@ export default function NavigationIcon({ open, setOpen }: NavigationIconProps) {
     >
       <svg
         className="pointer-events-none"
-        width={16}
-        height={16}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
         xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#fff"
+        viewBox="0 0 256 256"
       >
         <path
           d="M224 128a8 8 0 0 1-8 8H40a8 8 0 0 1 0-16h176a8 8 0 0 1 8 8"
