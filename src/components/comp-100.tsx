90b79bdebd3d93227a3320185866b72c2fa6a129
developerplus2025 <anpham08112009@gmail.com>
2025-06-23 20:10:25 +0700

    fix: update SVG properties in NavigationIcon and NavigationMobile components for improved rendering and accessibility

--- src/components/comp-100.tsx
+++ src/components/comp-100.tsx
@@ -16,20 +16,21 @@ export default function NavigationIcon({ open, setOpen }: NavigationIconProps) {
       aria-label={open ? "Close menu" : "Open menu"}
     >
       <svg
-        className="pointer-events-none"
         xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
         width="16"
         height="16"
-        fill="#fff"
-        viewBox="0 0 256 256"
       >
         <path
-          d="M224 128a8 8 0 0 1-8 8H40a8 8 0 0 1 0-16h176a8 8 0 0 1 8 8"
           className="ease-[cubic-bezier(.5,.85,.25,1.1)] origin-center -translate-y-[7px] transition-all duration-300 group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+          d="M1.75 4H1V5.5H1.75H14.25H15V4H14.25H1.75Z"
+          fill="currentColor"
         />
+
         <path
-          d="M40 184h176a8 8 0 0 0 0-16H40a8 8 0 0 0 0 16"
           className="ease-[cubic-bezier(.5,.85,.25,1.8)] origin-center transition-all duration-300 group-aria-expanded:rotate-45"
+          d="M1.75 10.5H1V12H1.75H14.25H15V10.5H14.25H1.75Z"
+          fill="currentColor"
         />
       </svg>
     </div>
