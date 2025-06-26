ccdc0aabdfeba067011b7ea7411983a948af0961
developerplus2025 <anpham08112009@gmail.com>
2025-06-26 16:08:33 +0700

    refactor: remove unused background style from Toaster component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -12,14 +12,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover)",
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
       {...props}
     />
-  )
+  );
 }
 
 export { Toaster }
