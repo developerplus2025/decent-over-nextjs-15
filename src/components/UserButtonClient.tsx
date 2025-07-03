7c19bff93518a223ed2a508b3ec5d33874642790
developerplus2025 <anpham08112009@gmail.com>
2025-07-03 14:08:50 +0700

    fix: correct className formatting in UserButtonClient for consistent styling

--- src/components/UserButtonClient.tsx
+++ src/components/UserButtonClient.tsx
@@ -132,7 +132,7 @@ export default function UserButtonClient() {
                 {...getFloatingProps()}
                 data-state={!isOpen ? "closed" : "open"}
                 data-side="right"
-                className="data-[state=open]:animate-in data data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 flex h-fit w-[16rem] origin-[50%_0%] flex-col justify-between rounded-xl border border-[#2c2c2c] bg-black"
+                className="data-[state=open]:animate-in data data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 flex h-fit w-[16rem] flex-col justify-between rounded-xl border border-[#2c2c2c] bg-black"
               >
                 <div className="flex w-full flex-col gap-2">
                   <div className="flex flex-col gap-2 px-4 py-2">
