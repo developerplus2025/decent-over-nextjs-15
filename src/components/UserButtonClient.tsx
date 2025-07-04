9ff489013f789933458b23da20c0eb3a0f259d65
developerplus2025 <anpham08112009@gmail.com>
2025-07-04 18:24:37 +0700

    cv

--- src/components/UserButtonClient.tsx
+++ src/components/UserButtonClient.tsx
@@ -138,10 +138,10 @@ export default function UserButtonClient() {
 						</svg>
 						<p className="text-xs"> Search for documention</p>
 						<div className="flex gap-1">
-							<kbd className=" text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border border-(--input) px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+							<kbd className=" text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border border-(--input) px-1.5 font-sans text-[10px] font-medium opacity-100 select-none">
 								<span className="text-xs">Ctrl</span>
 							</kbd>
-							<kbd className=" text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border border-(--input) px-1.5 font-mono text-[10px] font-medium opacity-100 select-none">
+							<kbd className=" text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border border-(--input) px-1.5 font-sans text-[10px] font-medium opacity-100 select-none">
 								<span className="text-xs">K</span>
 							</kbd>
 						</div>
