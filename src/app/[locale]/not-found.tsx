d6ca88e97bbd98d593ae7de098472c8546250e0a
developerplus2025 <anpham08112009@gmail.com>
2025-07-05 17:04:12 +0700

    cv

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,35 +1,4 @@
-import Link from "next/link";
-import React from "react";
+// Note that `app/[locale]/[...rest]/page.tsx`
+// is necessary for this page to render.
 
-export default function NotFound() {
-  return (
-    <div className="w-full flex justify-center mt-12">
-      <div className=" w-[900px] gap-8 h-[400px] flex flex-col items-center justify-center rounded-lg">
-        <p className="text-3xl"> Oops, page not found!</p>
-        <p>
-          The page you&apos;re looking for doesn&apos;t exist or has been moved.
-        </p>
-        <div className="flex gap-4">
-          <Link
-            href="/"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
-          >
-            Home
-          </Link>
-          <Link
-            href="/docs"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
-          >
-            Docs
-          </Link>
-          <Link
-            href="/pricing"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
-          >
-            Pricing
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-}
+export {default} from '@/components/NotFoundPage';
