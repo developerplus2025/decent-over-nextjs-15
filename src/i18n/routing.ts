3ec76b86c99f82b540dd2f72794f3bb7350f1ab9
developerplus2025 <anpham08112009@gmail.com>
2025-07-06 10:15:41 +0700

    cv

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,24 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "de"],
+  locales: [
+    "en", // English
+    "vi", // Vietnamese
+    "de", // German
+    "fr", // French
+    "es", // Spanish
+    "it", // Italian
+    "ja", // Japanese
+    "zh", // Chinese
+    "ko", // Korean
+    "ru", // Russian
+    "pt", // Portuguese
+    "hi", // Hindi
+    "th", // Thai
+    "id", // Indonesian
+    "ar", // Arabic
+  ],
+
   defaultLocale: "en",
   pathnames: {
     "/": "/",
