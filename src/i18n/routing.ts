1940f060fe7ea18ded239649e58b7b0ca193bf0d
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-07-06 11:47:08 +0700

    Update routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,7 @@
 export const routing = {
   locales: ["en", "vi", "de", "fr", "it", "ja", "zh", "ko", "ru"],
   defaultLocale: "en",
+localePrefix: 'as-needed',
   pathnames: {
     "/": "/",
     "/library": {
