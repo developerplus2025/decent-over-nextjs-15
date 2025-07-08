1288128b88b02834b98707d13d06b8abc74f816f
developerplus2025 <anpham08112009@gmail.com>
2025-07-08 20:58:39 +0700

    cv

--- src/app/[locale]/(home)/components/main-text-home.tsx
+++ src/app/[locale]/(home)/components/main-text-home.tsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import Modal from "./Modal";
 import { useTranslations } from "next-intl";
+import { ModalAnimationVideo } from "./modal-animation-video";
 export default function MainTextHome() {
   const { theme, systemTheme, setTheme } = useTheme();
   const [open, setOpen] = useState(false);
@@ -29,7 +30,7 @@ export default function MainTextHome() {
   const [value, setValue] = React.useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
   const t = useTranslations('MainTextHome');
-  const b = useTranslations('ButtonText');
+ 
   React.useEffect(() => {
     const interval = setInterval(() => {
       setValue((v) => (v >= 100 ? 0 : v + 10));
@@ -156,31 +157,8 @@ export default function MainTextHome() {
       </div>
       <div className="min-[300px]:flex min-[300px]:w-[200px] min-[300px]:flex-col min-[300px]:items-center min-[300px]:gap-[1rem] xl:flex xl:w-fit xl:flex-row xl:items-center xl:justify-center xl:gap-8">
         <DownloadButton />
-        <Modal isOpen={open} onClose={() => setOpen(false)} />
-        <Button
-          className="min-[300px]:w-full md:w-fit xl:w-fit"
-          onClick={() => setOpen(true)}
-          variant={"default"}
-        >
-          <svg
-            className="mr-2 h-4 w-4"
-            data-testid="geist-icon"
-            height={16}
-            strokeLinejoin="round"
-            viewBox="0 0 16 16"
-            width={16}
-            style={{ color: "currentcolor" }}
-          >
-            <path
-              fill="#666"
-              fillRule="evenodd"
-              d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6 11l5.5-3L6 5v6Z"
-              clipRule="evenodd"
-              style={{ fill: "currentColor" }}
-            />
-          </svg>
-          {b('WatchText')}
-        </Button>
+       <ModalAnimationVideo/>
+       
       </div>
     </motion.div>
   );
