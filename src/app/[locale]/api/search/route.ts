bcc551e1e63f37bd8213d7f0d88e6db7a1ef7165
developerplus2025 <anpham08112009@gmail.com>
2025-07-08 20:12:40 +0700

    cv

--- src/app/[locale]/api/search/route.ts
+++ src/app/[locale]/api/search/route.ts
@@ -1,11 +1,7 @@
-import { getSource } from "@/lib/source";
+import { source } from "@/lib/source";
 import { createFromSource } from "fumadocs-core/search/server";
 
-export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const pathname = url.pathname; // ví dụ: /en/api/search
-  const locale = pathname.split("/")[1]; // lấy 'en'
-
-  const source = getSource(locale);
-  return createFromSource(source).GET(request);
-}
+export const { GET } = createFromSource(source, {
+  // https://docs.orama.com/open-source/supported-languages
+  language: "english",
+});
