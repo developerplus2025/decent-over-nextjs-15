5f68f3d05ccdf4b83064b42cf25ba81d44b1cdc9
developerplus2025 <anpham08112009@gmail.com>
2025-07-09 14:35:48 +0700

    vb

--- src/components/UserButtonClient.tsx
+++ src/components/UserButtonClient.tsx
@@ -202,7 +202,7 @@ export default function UserButtonClient() {
              
               >
                 <div
-                  data-state={status === "open" ? 'open' : 'closed'}
+                  data-state={isOpen ? 'open' : 'closed'}
                   data-side="right"
                   className="data-[state=open]:animate-in data data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 flex h-fit w-[16rem] flex-col justify-between rounded-xl border border-[#2c2c2c] bg-black"
                 >
