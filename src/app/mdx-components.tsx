c5e11eedd030c14117c694f56ea2ebe0a36576fb
developerplus2025 <anpham08112009@gmail.com>
2025-07-13 15:10:52 +0700

    cv

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -3,5 +3,8 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    h1: ({ children }) => (
+      <h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>
+    ),
   }
 }
