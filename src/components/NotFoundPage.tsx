331092f337e0aa7dedff98de1199d647cf82b9f7
developerplus2025 <anpham08112009@gmail.com>
2025-07-14 15:21:11 +0700

    cv

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,33 +1,35 @@
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 export default function NotFoundPage() {
   return (
-    <div className="w-full flex justify-center mt-12">
+    <div className="w-full flex h-[calc(100vh-80px)] justify-center mt-12">
       <div className=" w-[900px] gap-8 h-[400px] flex flex-col items-center justify-center rounded-lg">
-        <p className="text-3xl"> Oops, page not found!</p>
-        <p>
+        <p className="text-[6rem] font-semibold">404</p>
+        <p className="w-[400px] font-medium">
           The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
         <div className="flex gap-4">
-          <Link
+          <Button> <Link
             href="/"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
+           
           >
             Home
-          </Link>
-          <Link
+          </Link></Button>
+         <Button> <Link
             href="/docs"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
+            
           >
             Docs
-          </Link>
-          <Link
+          </Link></Button><Button> <Link
             href="/pricing"
-            className="dark:bg-white px-3 py-1 rounded-md bg-black text-white  dark:text-black"
+           
           >
             Pricing
-          </Link>
+          </Link></Button>
+         
+         
         </div>
       </div>
     </div>
