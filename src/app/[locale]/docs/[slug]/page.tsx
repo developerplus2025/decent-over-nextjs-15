f42333d009dfc9b45c4b72def2d8f55753c96b5f
developerplus2025 <anpham08112009@gmail.com>
2025-07-15 20:30:54 +0700

    cv

--- src/app/[locale]/docs/[slug]/page.tsx
+++ src/app/[locale]/docs/[slug]/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
-  const { default: Post } = await import(`@/content/docs/${slug}.mdx`)
+  const { default: Post } = await import(`./content/docs/${slug}.mdx`)
  
   return <Post />
 }
