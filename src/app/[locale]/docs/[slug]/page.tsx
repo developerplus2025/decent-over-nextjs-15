2e8b7b47bb60214f0a8a7831f2d734b837404052
developerplus2025 <anpham08112009@gmail.com>
2025-07-15 20:36:09 +0700

    vb

--- src/app/[locale]/docs/[slug]/page.tsx
+++ src/app/[locale]/docs/[slug]/page.tsx
@@ -1,12 +1,11 @@
 // app/[locale]/docs/[slug]/page.tsx
-
 export default async function Page({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const { slug } = await params
-  const { default: Post } = await import(`./content/docs/${slug}.mdx`)
+  const { default: Post } = await import(`../content/docs/${slug}.mdx`)
  
   return <Post />
 }
