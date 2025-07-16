91183398aa1056a2c4e69a0d0e1e76cc15399ae4
developerplus2025 <anpham08112009@gmail.com>
2025-07-16 21:15:22 +0700

    cv

---
+++ src/app/[locale]/docs/[slug]/page.tsx
@@ -0,0 +1,17 @@
+// app/[locale]/docs/[slug]/page.tsx
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const { default: Post } = await import(`@/content/docs/${slug}.mdx`)
+ 
+  return <Post />
+}
+export function generateStaticParams() {
+  return [{slug:'index'}]
+}
+
+export const dynamicParams = true
