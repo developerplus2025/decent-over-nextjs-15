70c065dcc76d4cf53651491c16b2e2a264a73c41
developerplus2025 <anpham08112009@gmail.com>
2025-08-03 05:49:21 -0400

    one line code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,4 @@
-'use client';
+// Note that `app/[locale]/[...rest]/page.tsx`
+// is necessary for this page to render.
 
-import Error from 'next/error';
-
-// This page renders when a route like `/unknown.txt` is requested.
-// In this case, the layout at `app/[locale]/layout.tsx` receives
-// an invalid value as the `[locale]` param and calls `notFound()`.
-
-export default function GlobalNotFound() {
-  return (
-    <html lang="en">
-      <body>
-        <Error statusCode={404} />;
-      </body>
-    </html>
-  );
-}
+export {default} from '@/components/NotFoundPage';
