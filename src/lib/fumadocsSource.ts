875b19d5cdf8d777b70c5587317a300da14fe143
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-08-03 08:13:46 -0400

    Update fumadocsSource.ts

--- src/lib/fumadocsSource.ts
+++ src/lib/fumadocsSource.ts
@@ -1,6 +1,5 @@
 // lib/fumadocsSource.ts
 import { docs } from "@/.source";
-import { blogPosts } from "@/.source";
 import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
 
@@ -11,9 +10,4 @@ export function getSource(locale: string) {
   });
 }
 
-export function getBlog(locale: string) {
-  return loader({
-    baseUrl: `/${locale}/docs`, // hoặc /${locale}/blog nếu bạn định dùng blog
-    source: createMDXSource(blogPosts),
-  });
-}
+
