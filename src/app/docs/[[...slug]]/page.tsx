576446187b2d1edd6aded4bc031e5ec0b69b1565
developerplus2025 <anpham08112009@gmail.com>
2025-08-04 09:55:42 -0400

    cv

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -79,6 +79,53 @@ export default async function Page(props: {
         header: <div className="h-4 w-10"></div>,
       }}
     >
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-start justify-between">
+            <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">
+              {doc.title}
+            </h1>
+            <div className="docs-nav bg-background/80 border-border/50 fixed inset-x-0 bottom-0 isolate z-50 flex items-center gap-2 border-t px-6 py-4 backdrop-blur-sm sm:static sm:z-0 sm:border-t-0 sm:bg-transparent sm:px-0 sm:pt-1.5 sm:backdrop-blur-none">
+              <DocsCopyPage
+                // @ts-expect-error - revisit fumadocs types.
+                page={doc.content}
+                url={absoluteUrl(page.url)}
+              />
+              {neighbours.previous && (
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="extend-touch-target ml-auto size-8 shadow-none md:size-7"
+                  asChild
+                >
+                  <Link href={neighbours.previous.url}>
+                    <IconArrowLeft />
+                    <span className="sr-only">Previous</span>
+                  </Link>
+                </Button>
+              )}
+              {neighbours.next && (
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="extend-touch-target size-8 shadow-none md:size-7"
+                  asChild
+                >
+                  <Link href={neighbours.next.url}>
+                    <span className="sr-only">Next</span>
+                    <IconArrowRight />
+                  </Link>
+                </Button>
+              )}
+            </div>
+          </div>
+          {doc.description && (
+            <p className="text-muted-foreground text-[1.05rem] text-balance sm:text-base">
+              {doc.description}
+            </p>
+          )}
+        </div>
+      </div>
       <DocsBody>
         <div
           data-slot="docs"
@@ -87,74 +134,7 @@ export default async function Page(props: {
           <div className="flex min-w-0 flex-1 flex-col">
             <div className="h-(--top-spacing) shrink-0" />
             <div className="mx-auto flex w-full max-w-2xl min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300">
-              <div className="flex flex-col gap-2">
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-start justify-between">
-                    <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">
-                      {doc.title}
-                    </h1>
-                    <div className="docs-nav bg-background/80 border-border/50 fixed inset-x-0 bottom-0 isolate z-50 flex items-center gap-2 border-t px-6 py-4 backdrop-blur-sm sm:static sm:z-0 sm:border-t-0 sm:bg-transparent sm:px-0 sm:pt-1.5 sm:backdrop-blur-none">
-                      <DocsCopyPage
-                        // @ts-expect-error - revisit fumadocs types.
-                        page={doc.content}
-                        url={absoluteUrl(page.url)}
-                      />
-                      {neighbours.previous && (
-                        <Button
-                          variant="secondary"
-                          size="icon"
-                          className="extend-touch-target ml-auto size-8 shadow-none md:size-7"
-                          asChild
-                        >
-                          <Link href={neighbours.previous.url}>
-                            <IconArrowLeft />
-                            <span className="sr-only">Previous</span>
-                          </Link>
-                        </Button>
-                      )}
-                      {neighbours.next && (
-                        <Button
-                          variant="secondary"
-                          size="icon"
-                          className="extend-touch-target size-8 shadow-none md:size-7"
-                          asChild
-                        >
-                          <Link href={neighbours.next.url}>
-                            <span className="sr-only">Next</span>
-                            <IconArrowRight />
-                          </Link>
-                        </Button>
-                      )}
-                    </div>
-                  </div>
-                  {doc.description && (
-                    <p className="text-muted-foreground text-[1.05rem] text-balance sm:text-base">
-                      {doc.description}
-                    </p>
-                  )}
-                </div>
-                {links ? (
-                  <div className="flex items-center space-x-2 pt-4">
-                    {links?.doc && (
-                      <Badge asChild variant="secondary">
-                        <Link href={links.doc} target="_blank" rel="noreferrer">
-                          Docs <IconArrowUpRight />
-                        </Link>
-                      </Badge>
-                    )}
-                    {links?.api && (
-                      <Badge asChild variant="secondary">
-                        <Link href={links.api} target="_blank" rel="noreferrer">
-                          API Reference <IconArrowUpRight />
-                        </Link>
-                      </Badge>
-                    )}
-                  </div>
-                ) : null}
-              </div>
               <div className="w-full flex-1 *:data-[slot=alert]:first:mt-0">
-                {/* @ts-expect-error - revisit fumadocs types. */}
-                <p>{page.data.content}</p>
                 <MDX components={mdxComponents} />
               </div>
             </div>
