8c408c388c1fc5f37306bd2b4696e309d16e36f1
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-08-04 11:02:12 -0400

    Update layout.tsx

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -18,7 +18,6 @@ export default function Layout({ children }: { children: ReactNode }) {
           >
             <SidebarProvider className="3xl:fixed:container 3xl:fixed:px-3 min-h-min flex-1 items-start px-0 [--sidebar-width:220px] [--top-spacing:0] lg:grid lg:grid-cols-[var(--sidebar-width)_minmax(0,1fr)] lg:[--sidebar-width:240px] lg:[--top-spacing:calc(var(--spacing)*4)]">
               <DocsSidebar tree={source.pageTree} />
-              <div className="h-full w-full">{children}</div>
             </SidebarProvider>
           </div>
         ),
