2ae2406a93a770a1aaf2e9e7be0747b2081810e0
developerplus2025 <anpham08112009@gmail.com>
2025-08-05 00:11:31 -0400

    cv

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { getMDXComponents  } from "@/../mdx-components";
+import { mdxComponents } from "@/../mdx-components";
 import {
   IconArrowLeft,
   IconArrowRight,
@@ -129,7 +129,19 @@ export default async function Page(props: {
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-       <MDX components={getMDXComponents()} />
+        <div
+          data-slot="docs"
+          className="flex items-stretch text-[1.05rem] sm:text-[15px] xl:w-full"
+        >
+          <div className="flex min-w-0 flex-1 flex-col">
+            <div className="h-(--top-spacing) shrink-0" />
+            <div className="mx-auto flex w-full max-w-2xl min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300">
+              <div className="w-full flex-1 *:data-[slot=alert]:first:mt-0">
+                <MDX components={mdxComponents} />
+              </div>
+            </div>
+          </div>
+        </div>
       </DocsBody>
     </DocsPage>
   );
