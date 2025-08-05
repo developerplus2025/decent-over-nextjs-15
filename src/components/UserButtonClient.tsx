99b9465042637e47b8b00a021494dd4549f5d3b0
developerplus2025 <anpham08112009@gmail.com>
2025-08-05 08:48:26 -0400

    df

--- src/components/UserButtonClient.tsx
+++ src/components/UserButtonClient.tsx
@@ -120,6 +120,21 @@ export default function UserButtonClient() {
         transition={{ duration: 0.5, ease: "easeOut" }}
         className={`${!isPending ? "" : "pointer-events-none"} flex items-center gap-2`}
       >
+        <svg
+          onClick={() => {
+            console.log("Use Search");
+            setOpenSearch(true);
+          }}
+          className="size-4.5 shrink-0"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="#ffffff"
+          viewBox="0 0 256 256"
+        >
+          <title>Search Icon</title>
+          <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
+        </svg>
         <div
           onClick={() => {
             console.log("Use Search");
