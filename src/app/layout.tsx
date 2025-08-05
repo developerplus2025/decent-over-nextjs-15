d3d2452ee3c0dd49fef9cbe2686f5b974e584352
developerplus2025 <anpham08112009@gmail.com>
2025-08-05 08:52:44 -0400

    sd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,15 +73,6 @@ export default function RootLayout({
               defaultTheme="dark"
             >
               {" "}
-              <Navigation className="min-[300px]:hidden xl:block">
-                Navigation children content, or empty fragment if not needed
-                <></>
-              </Navigation>
-              <NavigationMobile className="xl:hidden">
-                {/* Add children here if needed */}
-                <></>
-              </NavigationMobile>
-              {/* <DocsNavigation /> */}
               <Toaster
                 icons={{
                   success: (
@@ -119,6 +110,14 @@ export default function RootLayout({
                   SearchDialog,
                 }}
               >
+                <Navigation className="min-[300px]:hidden xl:block">
+                  Navigation children content, or empty fragment if not needed
+                  <></>
+                </Navigation>
+                <NavigationMobile className="xl:hidden">
+                  {/* Add children here if needed */}
+                  <></>
+                </NavigationMobile>
                 <ViewTransition>{children}</ViewTransition>
               </RootProvider>
               <Footer />
