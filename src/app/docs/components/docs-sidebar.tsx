445dcc864c79fd957b8b94c7b6f4549956cadf04
developerplus2025 <anpham08112009@gmail.com>
2025-08-06 06:23:39 -0400

    df

--- src/app/docs/components/docs-sidebar.tsx
+++ src/app/docs/components/docs-sidebar.tsx
@@ -42,33 +42,36 @@ export function DocsSidebar({
         {tree.children.map((item) =>
           item.name === "Library Management" ? (
             <SidebarGroup key={item.$id}>
-              <Collapsible defaultOpen className="group/collapsible">
-                {item.type === "folder" && (
-                  <SidebarMenu className="gap-0.5">
-                    {item.children.map((item) => {
-                      return (
-                        item.type === "page" && (
-                          <SidebarMenuItem key={item.url}>
-                            <CollapsibleTrigger asChild>
-                              {item.name}
-                            </CollapsibleTrigger>
-                            <CollapsibleContent>
-                              {" "}
-                              <SidebarMenuButton
-                                asChild
-                                isActive={item.url === pathname}
-                                className="data-[active=true]:border-input hover:bg-input 3xl:fixed:w-full 3xl:fixed:max-w-48 relative h-[30px] w-fit overflow-visible border border-transparent text-[0.8rem] font-medium after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md data-[active=true]:bg-white dark:data-[active=true]:bg-black"
-                              >
-                                <Link href={item.url}>{item.name}</Link>
-                              </SidebarMenuButton>
-                            </CollapsibleContent>
-                          </SidebarMenuItem>
-                        )
-                      );
-                    })}
-                  </SidebarMenu>
-                )}
-              </Collapsible>
+              <SidebarMenu>
+                <Collapsible defaultOpen className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>{item.name}</SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      {item.type === "folder" && (
+                        <SidebarMenuSub className="gap-0.5">
+                          {item.children.map((item) => {
+                            return (
+                              item.type === "page" && (
+                                <SidebarMenuItem key={item.url}>
+                                  <SidebarMenuButton
+                                    asChild
+                                    isActive={item.url === pathname}
+                                    className="data-[active=true]:border-input hover:bg-input 3xl:fixed:w-full 3xl:fixed:max-w-48 relative h-[30px] w-fit overflow-visible border border-transparent text-[0.8rem] font-medium after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md data-[active=true]:bg-white dark:data-[active=true]:bg-black"
+                                  >
+                                    <Link href={item.url}>{item.name}</Link>
+                                  </SidebarMenuButton>
+                                </SidebarMenuItem>
+                              )
+                            );
+                          })}
+                        </SidebarMenuSub>
+                      )}
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
+              </SidebarMenu>
             </SidebarGroup>
           ) : (
             <SidebarGroup key={item.$id}>
