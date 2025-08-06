818c4feff188f78047840ae7ad6f27d00cdfc23c
developerplus2025 <anpham08112009@gmail.com>
2025-08-06 10:19:27 -0400

    df

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function VideoPage() {
   return (
     <div className="border-input flex h-[30rem] w-[70rem] rounded-xl border">
-      <div className="h-full w-[40rem]"></div>
+      <div className="h-full w-[90rem]"></div>
       <div className="divide-input grid h-full w-full grid-cols-2 border border-l"></div>
     </div>
   );
