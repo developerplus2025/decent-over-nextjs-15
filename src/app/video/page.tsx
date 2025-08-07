54b1beecb745bdd6b12018bc671caaffdcd960bc
developerplus2025 <anpham08112009@gmail.com>
2025-08-07 03:16:59 -0400

    df

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,12 +1,30 @@
 import { Slider } from "@/components/ui/slider";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
 
+const items = [
+  {
+    id: 1,
+    link: "",
+  },
+];
 export default function VideoPage() {
   return (
     <div className="border-input flex h-[30rem] w-[70rem] rounded-xl border">
-      <div className="h-full w-[90rem]"></div>
-      <div className="divide-input grid h-full w-full grid-cols-2 border border-l"></div>
+      <div
+        className="styled-scrollbar h-full w-[90rem]"
+        style={{ scrollbarWidth: "thin" }}
+      >
+        {items.map((items) => (
+          <iframe src={items.link} key={items.id}></iframe>
+        ))}
+      </div>
+      <div className="divide-input grid h-full w-full grid-cols-2 border border-l">
+        {items.map((items) => (
+          <Link key={items.id} href={`#${items.id}`}></Link>
+        ))}
+      </div>
     </div>
   );
 }
