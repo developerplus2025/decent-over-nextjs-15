dec9bdc66a15677641b2e00b78d67873c2e97b5a
developerplus2025 <anpham08112009@gmail.com>
2025-08-07 07:21:19 -0400

    sd

--- src/app/docs/components/docs-sidebar.tsx
+++ src/app/docs/components/docs-sidebar.tsx
@@ -78,7 +78,7 @@ export function DocsSidebar({
                       <CollapsibleContent className="">
                         {item.type === "folder" && (
                           <SidebarMenuSub className="gap-0.5 border-[#404040]">
-                            {item.children.map((item) => {
+                            {item.children.children.map((item) => {
                               return (
                                 item.type === "page" && (
                                   <SidebarMenuItem key={item.url}>
