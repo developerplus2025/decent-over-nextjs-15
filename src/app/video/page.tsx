35c6d9a0e41e4f5973214ffe7de65ec482b74f34
developerplus2025 <anpham08112009@gmail.com>
2025-08-08 03:16:32 -0400

    sd

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -6,7 +6,27 @@ import React from "react";
 const items = [
   {
     id: 1,
-    link: "",
+    link: "https://drive.google.com/file/d/1Igv3t5hzs4RkGpi_btn_6SllN-XTCTSH/preview",
+  },
+  {
+    id: 2,
+    link: "https://drive.google.com/file/d/16GJV_lbb_dIL-iv2ulykwnkd-L_fYf9e/preview",
+  },
+  {
+    id: 3,
+    link: "https://drive.google.com/file/d/1c7JOAUW1bzx1UTqA7x_NrEU3Ve7KfSBN/preview",
+  },
+  {
+    id: 4,
+    link: "https://drive.google.com/file/d/1YSpnhWHWznFkhUZlNSkt6O359GX2zL-l/preview",
+  },
+  {
+    id: 5,
+    link: "https://drive.google.com/file/d/1-uI-Kvu-iIDSKOYv3xVh51L3M3fvRgOg/preview",
+  },
+  {
+    id: 6,
+    link: "https://drive.google.com/file/d/1Igv3t5hzs4RkGpi_btn_6SllN-XTCTSH/preview",
   },
 ];
 export default function VideoPage() {
