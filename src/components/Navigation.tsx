582137953493ddef9a1aa6f28df3a992b88c6eec
developerplus2025 <anpham08112009@gmail.com>
2025-08-08 09:52:53 -0400

    sd

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -132,29 +132,7 @@ export default function Navigation({
                   </NavigationLink>
                 </NavigationMenuItem>
                 <NavigationMenuItem className="relative px-3">
-                  <NavigationLink href="/docs">
-                    Docs
-                    {pathname !== "/docs-root" && (
-                      <sup>
-                        <svg
-                          data-testid="geist-icon"
-                          height={12}
-                          strokeLinejoin="round"
-                          viewBox="0 0 15 15"
-                          width={12}
-                          style={{ color: "currentcolor" }}
-                        >
-                          <title>Arrow Up Right</title>
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M6.75011 4H6.00011V5.5H6.75011H9.43945L5.46978 9.46967L4.93945 10L6.00011 11.0607L6.53044 10.5303L10.499 6.56182V9.25V10H11.999V9.25V5C11.999 4.44772 11.5512 4 10.999 4H6.75011Z"
-                            fill="currentColor"
-                          />
-                        </svg>
-                      </sup>
-                    )}
-                  </NavigationLink>
+                  <NavigationLink href="/docs">Docs</NavigationLink>
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
