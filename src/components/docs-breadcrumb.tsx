4ed5e2a5473dc109940b4788a5efb0b0f1061bd8
developerplus2025 <anpham08112009@gmail.com>
2025-08-11 02:48:43 -0400

    sd

--- src/components/docs-breadcrumb.tsx
+++ src/components/docs-breadcrumb.tsx
@@ -10,44 +10,46 @@ export function DocsBreadcrumb({ tree }: { tree: PageTree.Root }) {
   const pathname = usePathname();
   const items = useBreadcrumb(pathname, tree);
  const path = pathname.split("/");
- const [part, setPart] = useState(1);
+ const [part, setPart] = useState(2);
  useEffect(() => {
    if (path.length === 2) {
      setPart(2);
    } else if (path.length === 3) {
      setPart(3);
    }
- }, [pathname]);
+ }, [pathname, path.length]);
  console.log(part);
-  if (items.length === 0) return null;
+ if (items.length === 0) return null;
 
-  return (
-    <div className="text-fd-muted-foreground -mb-3 flex flex-row items-center gap-1 text-sm font-medium">
-      <span className="truncate">Docs</span>{" "}
-      <ChevronRight className="size-4 shrink-0 rtl:rotate-180" />
-      {items.map((item, i) => (
-        <Fragment key={i}>
-          {i !== 0 && (
-            <ChevronRight
-              className={`${
-                part == 1 ? "" : "!hidden"
-              } size-4 shrink-0 rtl:rotate-180`}
-            />
-          )}
-          {item.url ? (
-            <Link
-              href={item.url}
-              className="hover:text-fd-accent-foreground truncate"
-            >
-              {item.name}
-            </Link>
-          ) : (
-            <span className={`${part == 1 ? "" : "!hidden"} truncate`}>
-              {item.name}
-            </span>
-          )}
-        </Fragment>
-      ))}
-    </div>
-  );
+ return (
+   <div className="text-fd-muted-foreground -mb-3 flex flex-row items-center gap-1 text-sm font-medium">
+     <span className="truncate">Docs</span>{" "}
+     <ChevronRight className="size-4 shrink-0 rtl:rotate-180" />
+     {items.map((item, i) => (
+       <Fragment key={i}>
+         {i !== 0 && part == 2 && (
+           <ChevronRight
+             className={`${
+               part == 2 ? "" : "!hidden"
+             } size-4 shrink-0 rtl:rotate-180`}
+           />
+         )}
+         {item.url ? (
+           <Link
+             href={item.url}
+             className="hover:text-fd-accent-foreground truncate"
+           >
+             {item.name}
+           </Link>
+         ) : (
+           part == 2 && (
+             <span className={`${part == 2 ? "" : "!hidden"} truncate`}>
+               {item.name}
+             </span>
+           )
+         )}
+       </Fragment>
+     ))}
+   </div>
+ );
 }
