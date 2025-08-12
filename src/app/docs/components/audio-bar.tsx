f6f27d87b2174f69751e4366fde1296a21654f94
developerplus2025 <anpham08112009@gmail.com>
2025-08-12 04:53:16 -0400

    df

--- src/app/docs/components/audio-bar.tsx
+++ src/app/docs/components/audio-bar.tsx
@@ -41,8 +41,8 @@ export default function AudioBar() {
           className={!play ? "hidden" : ""}
           onClick={() => handlePause()}
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
+          width="14"
+          height="14"
           fill="#ffffff"
           viewBox="0 0 256 256"
         >
