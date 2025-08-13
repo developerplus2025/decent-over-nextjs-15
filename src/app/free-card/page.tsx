306bda70ae0d2d7cdcf994e5b63eb0432cf3ce92
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-08-13 08:01:41 -0400

    Update page.tsx

--- src/app/free-card/page.tsx
+++ src/app/free-card/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function FreeCard() {
   return (
     <div className="w-screen text-center font-mono text-2xl font-bold">
-      Sắp Ra Mắt 20/07/2025
+      Mã thẻ cào điện thoại: 6138-9308-7574-377
     </div>
   );
 }
