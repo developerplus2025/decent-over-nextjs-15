ca17fa8115f48107f1accf16c4c1f862ac8eb298
developerplus2025 <anpham08112009@gmail.com>
2025-08-14 10:10:51 -0400

    df

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -204,7 +204,7 @@ import React, { useEffect, useRef, useState } from "react";
                xmlns="http://www.w3.org/2000/svg"
                width="16"
                height="16"
-               fill={`index == 0 ? "#a1a1a1" : "ffffff"`}
+               fill={`${index == 0 ? "#a1a1a1" : "ffffff"}`}
                viewBox="0 0 256 256"
              >
                <path d="M208,47.88V208.12a16,16,0,0,1-24.43,13.43L64,146.77V216a8,8,0,0,1-16,0V40a8,8,0,0,1,16,0v69.23L183.57,34.45A15.95,15.95,0,0,1,208,47.88Z"></path>
@@ -244,7 +244,7 @@ import React, { useEffect, useRef, useState } from "react";
                xmlns="http://www.w3.org/2000/svg"
                width="16"
                height="16"
-               fill={`index == 0 ? "#a1a1a1" : "ffffff"`}
+               fill={`${index == 0 ? "#a1a1a1" : "ffffff"}`}
                viewBox="0 0 256 256"
              >
                <path d="M208,40V216a8,8,0,0,1-16,0V146.77L72.43,221.55A15.95,15.95,0,0,1,48,208.12V47.88A15.95,15.95,0,0,1,72.43,34.45L192,109.23V40a8,8,0,0,1,16,0Z"></path>
