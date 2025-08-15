b13e0d01c9d8df51f777979d8c14962cea914ff3
Phạm Quang Trường An <139692078+developerplus2025@users.noreply.github.com>
2025-08-15 20:57:05 +0700

    Update navigation.tsx

--- src/app/mobile/components/navigation.tsx
+++ src/app/mobile/components/navigation.tsx
@@ -4,7 +4,7 @@ import UserButton from "./user-button";
 export default function Navigation() {
   return (
     <div className="flex h-[50px] items-center justify-between">
-      <div className="text-[1rem] font-bold">Music Listen</div>
+      <div className="text-[1rem] font-bold">Listen Music</div>
       <div className="flex items-center gap-[1rem]">
         <Menu />
         <UserButton />
