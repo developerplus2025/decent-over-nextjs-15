3d251d331c44bc33a92b8185a7cd716af8f7f8e1
developerplus2025 <anpham08112009@gmail.com>
2025-08-16 00:10:02 -0400

    sd

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,6 +14,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
         {
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(input)",
+          "-toast-close-button-transform": "translate(0%, 0%);",
         } as React.CSSProperties
       }
       {...props}
