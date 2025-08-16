db41503c9d26d4fb68a459028438da7844a6b3c3
developerplus2025 <anpham08112009@gmail.com>
2025-08-16 06:31:58 -0400

    df

--- src/app/docs/components/path-animation.tsx
+++ src/app/docs/components/path-animation.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { motion } from "framer-motion";
+
 export default function PathAnimation() {
   return (
     <motion.div
@@ -6,7 +9,7 @@ export default function PathAnimation() {
         width: 28,
         height: 28,
         background: "tomato",
-        offsetPath: 'path("M0 50 Q 50 0 100 50")', // simple arc
+        offsetPath: 'path("M0 50 Q 50 0 100 50")',
       }}
       animate={{ offsetDistance: ["100%", "0%", "100%"] }}
       transition={{ duration: 3, ease: "easeInOut", repeat: Infinity }}
