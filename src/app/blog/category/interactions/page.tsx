e2d89448825b75a240868e6c2e5116751b59d731
developerplus2025 <anpham08112009@gmail.com>
2025-08-17 23:24:37 -0400

    sd

--- src/app/blog/category/interactions/page.tsx
+++ src/app/blog/category/interactions/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Navigation } from "@/components/Navigation";
 import {
   Card,
   CardContent,
@@ -22,7 +21,7 @@ export default function BlogPage() {
   return (
     <main className="mb-[4rem] flex w-full flex-col items-center justify-center px-[10rem]">
       <div className="flex w-full items-center justify-between gap-[4rem] pt-[4rem]">
-        <h2 className="flex-shrink-0 text-nowrap text-2xl font-bold tracking-tight">
+        <h2 className="flex-shrink-0 text-2xl font-bold tracking-tight text-nowrap">
           Recent Articles
         </h2>
         <AnimatedTabs />
@@ -35,7 +34,7 @@ export default function BlogPage() {
           <MagnifyingGlassIcon
             width="21"
             height="21"
-            className="search_input-blog absolute left-[16px] top-1/2 -translate-y-1/2"
+            className="search_input-blog absolute top-1/2 left-[16px] -translate-y-1/2"
           />
         </div>
         <DatePickerBlog />
@@ -47,13 +46,13 @@ export default function BlogPage() {
             return (
               <Card
                 key={index}
-                className={`flex flex-col justify-between overflow-hidden rounded-none ${index === 0 ? "border-[0px] border-l border-t" : "border-[0px]"}`}
+                className={`flex flex-col justify-between overflow-hidden rounded-none ${index === 0 ? "border-[0px] border-t border-l" : "border-[0px]"}`}
               >
                 <CardHeader className="hidden p-2">
                   <div className="aspect-[2/1] w-full border-[0px] bg-black object-cover"></div>
                 </CardHeader>
                 <CardContent className="p-6">
-                  <div className="flex items-center justify-between gap-4 border-[0px] text-sm text-muted-foreground">
+                  <div className="text-muted-foreground flex items-center justify-between gap-4 border-[0px] text-sm">
                     <Badge className="border-0 p-0" variant="outline">
                       {post.data.category}
                     </Badge>
@@ -68,11 +67,11 @@ export default function BlogPage() {
                       {post.data.title}
                     </Link>
                   </h3>
-                  <p className="mt-4 line-clamp-[9] text-sm text-muted-foreground">
+                  <p className="text-muted-foreground mt-4 line-clamp-[9] text-sm">
                     {post.data.description}
                   </p>
                 </CardContent>
-                <CardFooter className="flex flex-col items-start gap-4 border-[0px] px-6 pb-6 pt-0">
+                <CardFooter className="flex flex-col items-start gap-4 border-[0px] px-6 pt-0 pb-6">
                   <div className="flex w-full items-center justify-between">
                     <Button variant="link" className="p-0" asChild>
                       <Link
