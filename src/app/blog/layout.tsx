33fa73f68571aa3b091fdaeafbc9515c15b8bce0
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 02:17:18 -0400

    df

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -26,7 +26,28 @@ export default function BlogLayout({
 }) {
   return (
     <section className="flex flex-col gap-[2rem]">
-      <AnimatedTabs />
+      <div className="flex items-center justify-between">
+        <div className="flex w-full items-center justify-between gap-[4rem] pt-[4rem]">
+          <h2 className="flex-shrink-0 text-2xl font-bold tracking-tight text-nowrap">
+            Recent Articles
+          </h2>
+          <AnimatedTabs />
+          <div className="relative w-full">
+            <Input
+              type="search"
+              placeholder="Search for favorite songs"
+              className="w-full rounded-full pl-[3rem] placeholder:text-[#7c7c7c]"
+            />
+            <MagnifyingGlassIcon
+              width="21"
+              height="21"
+              className="search_input-blog absolute top-1/2 left-[16px] -translate-y-1/2"
+            />
+          </div>
+          <DatePickerBlog />
+        </div>
+        <AnimatedTabs />
+      </div>
       {children}
     </section>
   );
--- src/app/docs/components/block-theme.tsx
+++ src/app/docs/components/block-theme.tsx
@@ -1,10 +1,16 @@
 "use client"
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function BlockTheme() {
-    const [theme,setTheme] = useState()
-    return (
-      <div className={`h-[80px] w-[300px]`}>
+  const [theme, setTheme] = useState("dark");
+  return (
+    <div className={`relative h-[80px] w-[300px]`}>
+      <div
+        className="absolute h-full w-full bg-black"
+        style={{ clipPath: "inset(0 0 0 0)" }}
+      ></div>
+      <div className="z-[10]">
         <h1>Experiesdnce the theme switch animation yourself.</h1>
         <p>
           This technique is using clip-path, this element is duplicated and
@@ -13,5 +19,13 @@ export default function BlockTheme() {
           the clip-path property.
         </p>
       </div>
-    );
+      <div>
+        <Button
+          onClick={() => {
+            setTheme(theme == "dark" ? "light" : "dark");
+          }}
+        ></Button>
+      </div>
+    </div>
+  );
 }
