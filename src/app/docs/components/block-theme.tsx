3e1cd3f8054c7904ac18160c64c9bab31290b394
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 02:24:52 -0400

    df

--- src/app/docs/components/block-theme.tsx
+++ src/app/docs/components/block-theme.tsx
@@ -3,13 +3,19 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export default function BlockTheme() {
+  const [change, setChange] = useState("no-change");
   const [theme, setTheme] = useState("dark");
   return (
     <div className={`relative h-[80px] w-[300px]`}>
-      <div
-        className="absolute h-full w-full bg-black"
-        style={{ clipPath: "inset(0 0 0 0)" }}
-      ></div>
+      {change === "yes" && (
+        <div
+          className="theme-switcher_clipPathReveal__l8VbV absolute inset-0 h-full w-full bg-black"
+          style={{ clipPath: "inset(0 0 0 0)" }}
+        ></div>
+      )}
+      {change === "no" && (
+        <div className="theme-switcher_clipPathReveal__l8VbV absolute inset-0 h-full w-full bg-white"></div>
+      )}
       <div className="z-[10]">
         <h1>Experiesdnce the theme switch animation yourself.</h1>
         <p>
@@ -22,6 +28,7 @@ export default function BlockTheme() {
       <div>
         <Button
           onClick={() => {
+            setChange("no");
             setTheme(theme == "dark" ? "light" : "dark");
           }}
         ></Button>
