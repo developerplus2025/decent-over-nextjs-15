858c2a175e6106f1d140afb810ab08c5aebff21e
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 22:10:57 -0400

    xc

--- src/app/blog/AnimationTab.tsx
+++ src/app/blog/AnimationTab.tsx
@@ -15,58 +15,72 @@ const normalizePath = (path: string) => path.replace(/\/$/, "");
 
 export function AnimatedTabs() {
   const path = usePathname();
-  const [index, setIndex] = useState();
+   const pathname = path.slice(1).split("/");
+   const arraypath = pathname;
+   const [show, setShow] = useState(() => {
+     if (arraypath.length == 2) {
+       return false;
+     } else if (arraypath.length == 3) {
+       return true;
+     } else {
+       return true;
+     }
+   });
 
-  const getInitialIndex = (path: string) => {
-    if (path === "/blog") {
-      return 0;
-    } else if (path === "/blog/category/plugins") {
-      return 1;
-    } else if (path === "/blog/category/production-tips") {
-      return 2;
-    } else {
-      return 3;
-    }
-  };
+   const getInitialIndex = (path: string) => {
+     if (path === "/blog") {
+       return 0;
+     } else if (path === "/blog/category/plugins") {
+       return 1;
+     } else if (path === "/blog/category/production-tips") {
+       return 2;
+     } else {
+       return 3;
+     }
+   };
 
-  const [activeTab, setActiveTab] = useState(TABS[getInitialIndex(path)].id);
-  useEffect(() => {
-    setActiveTab(TABS[getInitialIndex(path)].id);
-  }, [path]);
+   const [activeTab, setActiveTab] = useState(TABS[getInitialIndex(path)].id);
+   useEffect(() => {
+     setActiveTab(TABS[getInitialIndex(path)].id);
+   }, [path]);
 
-  const pathname = path.slice(0).split("/");
-  const arraypath = pathname;
-  console.log(arraypath);
+   useEffect(() => {
+     const pathname = path.slice(1).split("/");
+     const arraypath = pathname;
+     if (arraypath.length == 2) {
+     }
+   });
 
   const router = useRouter();
   return (
     <div className="flex space-x-1">
-      {TABS.map((tab) => (
-        <Link href={`/blog/${tab.link}`} key={tab.id}>
-          <button
-            key={tab.id}
-            onClick={() => {
-              setActiveTab(tab.id);
-            }}
-            className={`${
-              activeTab === tab.id ? "" : "hover:text-white/60"
-            } relative flex h-8 items-center justify-center rounded-full p-3 text-sm font-medium text-nowrap text-white outline-sky-400 transition focus-visible:outline-2`}
-            style={{
-              WebkitTapHighlightColor: "transparent",
-            }}
-          >
-            {activeTab === tab.id && (
-              <motion.span
-                layoutId="bubble"
-                className="absolute inset-0 z-10 bg-white mix-blend-difference"
-                style={{ borderRadius: 9999 }}
-                transition={{ duration: 0.25 }}
-              />
-            )}
-            {tab.label}
-          </button>
-        </Link>
-      ))}
+      {show &&
+        TABS.map((tab) => (
+          <Link href={`/blog/${tab.link}`} key={tab.id}>
+            <button
+              key={tab.id}
+              onClick={() => {
+                setActiveTab(tab.id);
+              }}
+              className={`${
+                activeTab === tab.id ? "" : "hover:text-white/60"
+              } relative flex h-8 items-center justify-center rounded-full p-3 text-sm font-medium text-nowrap text-white outline-sky-400 transition focus-visible:outline-2`}
+              style={{
+                WebkitTapHighlightColor: "transparent",
+              }}
+            >
+              {activeTab === tab.id && (
+                <motion.span
+                  layoutId="bubble"
+                  className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                  style={{ borderRadius: 9999 }}
+                  transition={{ duration: 0.25 }}
+                />
+              )}
+              {tab.label}
+            </button>
+          </Link>
+        ))}
     </div>
   );
 }
