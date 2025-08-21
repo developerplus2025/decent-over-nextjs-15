a055fd74f9a5536975f3721f906ef3c0a5427848
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 22:45:56 -0400

    xc

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -55,9 +55,34 @@ export async function generateMetadata(props: {
   return {
     title: doc.title,
     description: doc.description,
+    openGraph: {
+      title: doc.title,
+      description: doc.description,
+      type: "article",
+      url: absoluteUrl(page.url),
+      images: [
+        {
+          url: `/og?title=${encodeURIComponent(
+            doc.title,
+          )}&description=${encodeURIComponent(doc.description)}`,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: doc.title,
+      description: doc.description,
+      images: [
+        {
+          url: `/og?title=${encodeURIComponent(
+            doc.title,
+          )}&description=${encodeURIComponent(doc.description)}`,
+        },
+      ],
+      creator: "@neurotune",
+    },
   };
 }
-
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
