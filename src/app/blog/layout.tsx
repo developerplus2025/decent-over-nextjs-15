9d3cb5c21f448a5913d2157628e45badf386eb87
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 22:23:46 -0400

    cv

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
@@ -15,61 +14,17 @@ import { cn } from "@/lib/utils";
 import { DatePickerBlog } from "./date-picker-blog";
 // import Tabs from "./tabs";
 import TabAnimation from "./tabs";
-import { AnimatedTabs } from "./AnimationTab";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+
+import NavBlog from "./nav-blog";
 export const metadata: Metadata = {
   title: "Blog - Decent",
   description: "Blog - Decent",
 };
-export default function BlogLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const path = usePathname();
-  const pathname = path.slice(1).split("/");
-  const arraypath = pathname;
-  const [show, setShow] = useState(() => {
-    if (arraypath.length == 2) {
-      return false;
-    } else if (arraypath.length == 3) {
-      return true;
-    } else {
-      return true;
-    }
-  });
-  useEffect(() => {
-    if (arraypath.length == 2) {
-      setShow(false);
-    } else if (arraypath.length == 3) {
-      setShow(true);
-    } else {
-      setShow(true);
-    }
-  }, [arraypath.length]);
+export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="flex flex-col gap-[3rem]">
       <div className="flex items-center justify-between px-[10rem]">
-        <div className="flex w-full items-center justify-between gap-[4rem] pt-[3rem]">
-          <h2 className="flex-shrink-0 text-2xl font-bold tracking-tight text-nowrap">
-            Recent Articles
-          </h2>
-          <AnimatedTabs />
-          <div className="relative w-full">
-            <Input
-              type="search"
-              placeholder="Search for favorite songs"
-              className="w-full rounded-full pl-[3rem] placeholder:text-[#7c7c7c]"
-            />
-            <MagnifyingGlassIcon
-              width="21"
-              height="21"
-              className="search_input-blog absolute top-1/2 left-[16px] -translate-y-1/2"
-            />
-          </div>
-          <DatePickerBlog />
-        </div>
+        <NavBlog />
       </div>
       {children}
     </section>
