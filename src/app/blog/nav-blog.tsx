b3a2e79f1829f6cc283103a7cb6ff888a470b68a
developerplus2025 <anpham08112009@gmail.com>
2025-08-20 22:24:32 -0400

    xc

--- src/app/blog/nav-blog.tsx
+++ src/app/blog/nav-blog.tsx
@@ -30,25 +30,27 @@ export default function NavBlog() {
   }, [arraypath.length]);
   return (
     <div>
-      <div className="flex w-full items-center justify-between gap-[4rem] pt-[3rem]">
-        <h2 className="flex-shrink-0 text-2xl font-bold tracking-tight text-nowrap">
-          Recent Articles
-        </h2>
-        <AnimatedTabs />
-        <div className="relative w-full">
-          <Input
-            type="search"
-            placeholder="Search for favorite songs"
-            className="w-full rounded-full pl-[3rem] placeholder:text-[#7c7c7c]"
-          />
-          <MagnifyingGlassIcon
-            width="21"
-            height="21"
-            className="search_input-blog absolute top-1/2 left-[16px] -translate-y-1/2"
-          />
+      {show && (
+        <div className="flex w-full items-center justify-between gap-[4rem] pt-[3rem]">
+          <h2 className="flex-shrink-0 text-2xl font-bold tracking-tight text-nowrap">
+            Recent Articles
+          </h2>
+          <AnimatedTabs />
+          <div className="relative w-full">
+            <Input
+              type="search"
+              placeholder="Search for favorite songs"
+              className="w-full rounded-full pl-[3rem] placeholder:text-[#7c7c7c]"
+            />
+            <MagnifyingGlassIcon
+              width="21"
+              height="21"
+              className="search_input-blog absolute top-1/2 left-[16px] -translate-y-1/2"
+            />
+          </div>
+          <DatePickerBlog />
         </div>
-        <DatePickerBlog />
-      </div>
+      )}
     </div>
   );
 }
