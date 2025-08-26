ca118791ad3666e9ba822d8ef665a292392f06ec
developerplus2025 <anpham08112009@gmail.com>
2025-08-26 04:06:22 -0400

    df

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,7 +5,7 @@ import PageTree from "./components/page-tree";
 
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
-import { Sidebar } from "@/src/components/sidebar";
+import { Sidebar, SidebarContent } from "@/src/components/sidebar";
 import { DocsLayout } from "@/components/layout/docs";
 import { docsOptions } from "@/lib/layout.shared";
 
@@ -61,15 +61,15 @@ export default function Layout({ children }: { children: ReactNode }) {
           },
         ],
 
-        // component: (
-        //   <div
-        //     className={cn(
-        //       "container-wrapper xl:border-input flex flex-col [--fd-sidebar-width:0px] xl:!w-[20rem] xl:border-r",
-        //     )}
-        //   >
-        //     <Sidebar Content />
-        //   </div>
-        // ),
+        component: (
+          <div
+            className={cn(
+              "container-wrapper xl:border-input flex flex-col [--fd-sidebar-width:0px] xl:!w-[20rem] xl:border-r",
+            )}
+          >
+            <SidebarContent />
+          </div>
+        ),
       }}
     >
       <PageTree />
