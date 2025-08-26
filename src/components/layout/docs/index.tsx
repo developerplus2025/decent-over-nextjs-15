f00a9572d1f5ce92d7f280c7f61d3fcb8e4f3bf3
developerplus2025 <anpham08112009@gmail.com>
2025-08-25 23:25:26 -0400

    sd

--- src/components/layout/docs/index.tsx
+++ src/components/layout/docs/index.tsx
@@ -321,7 +321,7 @@ export function DocsLayout({
             sidebarEnabled && sidebarVariables,
             !nav.component &&
               nav.enabled !== false &&
-              "[--fd-nav-height:58.8px] md:[--fd-nav-height:58.8px]",
+              "md:[--fd-nav-height:58.8px] xl:[--fd-nav-height:0px]",
             props.containerProps?.className,
           )}
         >
