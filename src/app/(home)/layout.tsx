d6d2fe7308189c4910a0e24adf5a801f0f4e24c3
developerplus2025 <anpham08112009@gmail.com>
2025-08-26 04:17:32 -0400

    df

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { GeistSans } from "geist/font/sans";
 import Image from "next/image";
+import { PageTree } from "../docs/components/page-tree";
+import { source } from "@/lib/source";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Decent: A Useful Platform For Musicians",
@@ -13,5 +15,11 @@ export default function HomeLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <section>
+      {" "}
+      <PageTree tree={source.pageTree} />
+      {children}
+    </section>
+  );
 }
