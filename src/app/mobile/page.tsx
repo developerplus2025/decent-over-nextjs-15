17e2fc9532bb5548d966368b562e35b43cdc12ce
developerplus2025 <anpham08112009@gmail.com>
2025-08-29 10:45:24 +0700

    df

--- src/app/mobile/page.tsx
+++ src/app/mobile/page.tsx
@@ -74,12 +74,13 @@ type TabKey =
   | "listeningMusic"
   | "libraryMusic"
   | "artistMusic"
-  | "accountSetting";
+  | "accountSetting"
+  | "historyMusic";
 
 type Tab = {
   id: TabKey;
   label: string;
-  icon?: JSX.Element;
+  icon: JSX.Element;
 };
 
 const tabs: Tab[] = [
@@ -143,13 +144,29 @@ const tabs: Tab[] = [
       </svg>
     ),
   },
+  {
+    id: "historyMusic",
+    label: " History Music",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#f0f0f0"
+        viewBox="0 0 256 256"
+      >
+        <path d="M136,80v43.47l36.12,21.67a8,8,0,0,1-8.24,13.72l-40-24A8,8,0,0,1,120,128V80a8,8,0,0,1,16,0Zm88-24a8,8,0,0,0-8,8V82c-6.35-7.36-12.83-14.45-20.12-21.83a96,96,0,1,0-2,137.7,8,8,0,0,0-11-11.64A80,80,0,1,1,184.54,71.4C192.68,79.64,199.81,87.58,207,96H184a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V64A8,8,0,0,0,224,56Z"></path>
+      </svg>
+    ),
+  },
 ];
 
-const contents: Record<TabKey, JSX.Element> = {
+const contents: Record<TabKey, JSX.Element | ""> = {
   listeningMusic: <ListeningMusic />,
   libraryMusic: <LibraryMusic />,
   artistMusic: <ArtistMusic />,
   accountSetting: <AccountSetting />,
+  historyMusic: "",
 };
 
 export default function MobilePage() {
@@ -160,7 +177,9 @@ export default function MobilePage() {
       <div className="border-input h-[50px] border-b px-[1rem]">
         <Navigation />
       </div>
-      <div className="h-[calc(100dvh-112px)]">{contents[activeTab]}</div>
+      <div className="h-[calc(100dvh-112px)]">
+        {contents[activeTab] == "" ? "Comming Soon" : contents[activeTab]}
+      </div>
       <div className="border-input flex h-[60px] items-center justify-between border-t px-[2rem]">
         {tabs.map((item) => (
           <div key={item.id} onClick={() => setActiveTab(item.id)}>
