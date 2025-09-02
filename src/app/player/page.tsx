67f9022accd6cedd09544dba395bf052a7bfacfa
developerplus2025 <anpham08112009@gmail.com>
2025-09-02 11:14:07 +0700

    we

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -3,7 +3,7 @@ import { LinearMediaPlayer } from "@/components/linear-player/components/media-p
 export default function Page() {
   return (
     <section className="dark flex h-dvh w-dvw bg-black">
-      <LinearMediaPlayer src="/854224-hd_1280_720_30fps.mp4" />
+      <LinearMediaPlayer src="https://decent-over.vercel.app/videos/854224-hd_1280_720_30fps.mp4" />
     </section>
   );
 }
