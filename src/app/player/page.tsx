25c1972c4d96d550c92f2e576e0515af3e3b17fc
developerplus2025 <anpham08112009@gmail.com>
2025-09-03 14:08:31 +0700

    df

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -3,7 +3,7 @@ import { LinearMediaPlayer } from "@/components/linear-player/components/media-p
 export default function Page() {
   return (
     <section className="dark flex h-dvh w-dvw bg-black">
-      <LinearMediaPlayer src="https://decent-over.vercel.app/videos/kevin-full.4CE3C73C-6032-4726-A296-E0AD9392874F.mp4" />
+      <LinearMediaPlayer src="https://static.linear.app/assets/web/quality/kevin-full.4CE3C73C-6032-4726-A296-E0AD9392874F.mp4" />
     </section>
   );
 }
