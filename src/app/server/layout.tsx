38ba505ed61c2bf6988941ebcde911de6a7c1ae0
developerplus2025 <anpham08112009@gmail.com>
2025-09-06 20:46:07 +0700

    sd

--- src/app/server/layout.tsx
+++ src/app/server/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from "next";
+import ServerPage from "./page";
+import { Suspense } from "react";
+import { Loader } from "@/components/ui/loader";
 export const metadata: Metadata = {
   title: "Server | Decent",
   description: "Decent - Server",
@@ -9,8 +12,10 @@ export default function ResourcesLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col gap-[6rem] items-center justify-center pt-28">
-      {children}
+    <section className="flex flex-col items-center justify-center gap-[6rem] pt-28">
+      <Suspense fallback={<Loader variant="classic" />}>
+        <ServerPage />
+      </Suspense>
     </section>
   );
 }
