8196fe1ca8bf8d3d4356c13b646629589951850a
developerplus2025 <anpham08112009@gmail.com>
2025-09-06 20:53:58 +0700

    sd

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -41,43 +41,42 @@ const PaginationItems = [
   },
 ];
 function ServerPage() {
-   const [rank, setRank] = useQueryState(
-     "count",
-     parseAsInteger.withDefault(1),
-   );
-  return (
-    <div className="flex flex-col gap-[6rem]">
-      <div className="grid grid-cols-3 place-items-center justify-items-center">
-        {Data.map(
-          (data) =>
-            data.name == Number(rank) && (
-              <div
-                key={data.rank}
-                className="border-input h-[400px] w-[400px] rounded-xl border bg-[#0c0c0c]"
-              >
-                {data.name}
-              </div>
-            ),
-        )}
-      </div>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          {PaginationItems.map((data) => (
-            <PaginationItem key={data.rank}>
-              <PaginationLink href={String(data.rank)}>{data.rank}</PaginationLink>
-            </PaginationItem>
-          ))}
+   const [rank, setRank] = useQueryState("page", parseAsInteger.withDefault(1));
+   return (
+     <div className="flex flex-col gap-[6rem]">
+       <div className="grid grid-cols-3 place-items-center justify-items-center">
+         {Data.map(
+           (data) =>
+             data.name == Number(rank) && (
+               <div
+                 key={data.rank}
+                 className="border-input h-[400px] w-[400px] rounded-xl border bg-[#0c0c0c]"
+               >
+                 {data.name}
+               </div>
+             ),
+         )}
+       </div>
+       <Pagination>
+         <PaginationContent>
+           <PaginationItem>
+             <PaginationPrevious onClick={() => setRank(Number(rank) - 1)} />
+           </PaginationItem>
+           {PaginationItems.map((data) => (
+             <PaginationItem key={data.rank}>
+               <PaginationLink href={`/server?page=${String(data.rank)}`}>
+                 {data.rank}
+               </PaginationLink>
+             </PaginationItem>
+           ))}
 
-          <PaginationItem>
-            <PaginationNext onClick={() => setRank(Number(rank) +1)} />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-    </div>
-  );
+           <PaginationItem>
+             <PaginationNext onClick={() => setRank(Number(rank) + 1)} />
+           </PaginationItem>
+         </PaginationContent>
+       </Pagination>
+     </div>
+   );
 }
 
 export default ServerPage
