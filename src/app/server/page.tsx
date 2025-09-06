e9621f30b4f3b164364c6209e272e0c6d4793568
developerplus2025 <anpham08112009@gmail.com>
2025-09-06 21:13:51 +0700

    sd

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { useRouter } from "next/router";
 import { parseAsInteger, useQueryState } from 'nuqs';
 import React from 'react'
 const Data = [
@@ -52,45 +53,51 @@ const PaginationItems = [
   },
 ];
 function ServerPage() {
-   const [rank, setRank] = useQueryState("page", parseAsInteger.withDefault(1));
-   return (
-     <div className="mb-[3rem] flex flex-col gap-[6rem]">
-       <div className="grid grid-cols-3 place-items-center justify-items-center gap-[2rem]">
-         {Data.map(
-           (data) =>
-             data.rank == Number(rank) && (
-               <div
-                 key={data.rank}
-                 className="border-input flex h-[400px] w-[400px] items-center justify-center rounded-xl border bg-[#0c0c0c]"
-               >
-                 <p className="text-3xl">{data.name}</p>
-               </div>
-             ),
-         )}
-       </div>
-       <Pagination>
-         <PaginationContent>
-           <PaginationItem>
-             <PaginationPrevious onClick={() => setRank(Number(rank) - 1)} />
-           </PaginationItem>
-           {PaginationItems.map((data) => (
-             <PaginationItem key={data.rank}>
-               <PaginationLink
-                 isActive={data.rank == rank ? true : false}
-                 href={`/server?page=${String(data.rank)}`}
-               >
-                 {data.rank}
-               </PaginationLink>
-             </PaginationItem>
-           ))}
+    const router = useRouter();
+    const [rank, setRank] = useQueryState(
+      "page",
+      parseAsInteger.withDefault(1),
+    );
+    return (
+      <div className="mb-[3rem] flex flex-col gap-[6rem]">
+        <div className="grid grid-cols-3 place-items-center justify-items-center gap-[2rem]">
+          {Data.map(
+            (data) =>
+              data.rank == Number(rank) && (
+                <div
+                  key={data.rank}
+                  className="border-input flex h-[400px] w-[400px] items-center justify-center rounded-xl border bg-[#0c0c0c]"
+                >
+                  <p className="text-3xl">{data.name}</p>
+                </div>
+              ),
+          )}
+        </div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => setRank(Number(rank) - 1)} />
+            </PaginationItem>
+            {PaginationItems.map((data) => (
+              <PaginationItem key={data.rank}>
+                <PaginationLink
+                  isActive={data.rank == rank ? true : false}
+                  onClick={() =>
+                    router.push(`/server?page=${String(data.rank)}`)
+                  }
+                >
+                  {data.rank}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
 
-           <PaginationItem>
-             <PaginationNext onClick={() => setRank(Number(rank) + 1)} />
-           </PaginationItem>
-         </PaginationContent>
-       </Pagination>
-     </div>
-   );
+            <PaginationItem>
+              <PaginationNext onClick={() => setRank(Number(rank) + 1)} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    );
 }
 
 export default ServerPage
