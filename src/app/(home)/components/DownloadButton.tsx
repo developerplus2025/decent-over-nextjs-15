36e7e6ab5f0ae57d1a60abfbf2ec169e54869f3b
developerplus2025 <anpham08112009@gmail.com>
2025-09-13 10:37:20 +0700

    df

--- src/app/(home)/components/DownloadButton.tsx
+++ src/app/(home)/components/DownloadButton.tsx
@@ -17,8 +17,8 @@ const DownloadButton: React.FC = () => {
   };
   return (
     <Button className="group" variant={"outline"} onClick={handleClick}>
-      <div className="flex h-[1rem] w-[2rem] flex-col items-center overflow-y-hidden">
-        <ArrowDownToLine className="mr-2 h-4 w-4 transition-transform duration-500 ease-out group-hover:-translate-y-[2rem]" />
+      <div className="flex h-[1rem] w-[2rem] flex-col items-center overflow-y-hidden transition-transform duration-500 ease-out group-hover:-translate-y-[2rem]">
+        <ArrowDownToLine className="mr-2 h-4 w-4" />
         <ArrowDownToLine className="mr-2 h-4 w-4" />
       </div>
       Download for Windows
