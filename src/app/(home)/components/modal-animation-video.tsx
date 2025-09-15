de5faf1c7de8097a78b87b8ce29ec94aa6bee0c7
developerplus2025 <anpham08112009@gmail.com>
2025-09-15 11:10:55 +0700

    er

--- src/app/(home)/components/modal-animation-video.tsx
+++ src/app/(home)/components/modal-animation-video.tsx
@@ -20,23 +20,42 @@ export const ModalAnimationVideo = () => {
           className="min-[300px]:w-full md:w-fit xl:w-fit"
           variant={"default"}
         >
-          <svg
-            className="mr-2 h-4 w-4"
-            data-testid="geist-icon"
-            height={16}
-            strokeLinejoin="round"
-            viewBox="0 0 16 16"
-            width={16}
-            style={{ color: "currentcolor" }}
-          >
-            <path
-              fill="#666"
-              fillRule="evenodd"
-              d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6 11l5.5-3L6 5v6Z"
-              clipRule="evenodd"
-              style={{ fill: "currentColor" }}
-            />
-          </svg>
+          <div className="flex h-[1rem] w-[2rem] flex-col items-center overflow-y-hidden">
+            <svg
+              className="mr-2 h-4 w-4 transition-transform duration-500 ease-out group-hover:-translate-y-[2rem]"
+              data-testid="geist-icon"
+              height={16}
+              strokeLinejoin="round"
+              viewBox="0 0 16 16"
+              width={16}
+              style={{ color: "currentcolor" }}
+            >
+              <path
+                fill="#666"
+                fillRule="evenodd"
+                d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6 11l5.5-3L6 5v6Z"
+                clipRule="evenodd"
+                style={{ fill: "currentColor" }}
+              />
+            </svg>
+            <svg
+              className="groud-hover:top-0 mr-2 h-4 w-4 transition-transform duration-500 ease-out group-hover:-translate-y-[1rem]"
+              data-testid="geist-icon"
+              height={16}
+              strokeLinejoin="round"
+              viewBox="0 0 16 16"
+              width={16}
+              style={{ color: "currentcolor" }}
+            >
+              <path
+                fill="#666"
+                fillRule="evenodd"
+                d="M14.5 8a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6 11l5.5-3L6 5v6Z"
+                clipRule="evenodd"
+                style={{ fill: "currentColor" }}
+              />
+            </svg>
+          </div>
           Watch Demo Now
         </Button>
       </DialogTrigger>
