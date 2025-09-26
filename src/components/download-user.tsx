94946288d411982cafa53e02501038392942a87a
developerplus2025 <anpham08112009@gmail.com>
2025-09-26 21:26:57 +0700

    df

--- src/components/download-user.tsx
+++ src/components/download-user.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 function DownloadUser() {
-  return redirect("/downloads/libre_desktop_version_11.9.4_free_windows.exe");
+  const router = useRouter();
+  const handleClick = async () => {
+    // Chuyển trang và chờ quá trình chuyển hoàn tất
+    await router.push(
+      "/downloads/libre_desktop_version_11.9.4_free_windows.exe",
+    );
+  };
 }
 
 export default DownloadUser
