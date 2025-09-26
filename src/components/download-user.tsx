0c997ff8d34b0bf6a0145073f47961b60e45efcb
developerplus2025 <anpham08112009@gmail.com>
2025-09-26 21:35:09 +0700

    fg

--- src/components/download-user.tsx
+++ src/components/download-user.tsx
@@ -1,14 +1,28 @@
 "use client";
-import React from "react";
-import { redirect, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { redirect, usePathname, useRouter } from "next/navigation";
 function DownloadUser() {
-  const router = useRouter();
-  const handleClick = async () => {
-    // Chuyển trang và chờ quá trình chuyển hoàn tất
-    await router.push(
-      "/downloads/libre_desktop_version_11.9.4_free_windows.exe",
-    );
-  };
+  const pathname = usePathname();
+  useEffect(() => {
+    // Chờ 5 giây sau khi trang tải xong và thực hiện tải file
+    const timer = setTimeout(() => {
+      // URL của file cần tải
+      const fileUrl =
+        "/downloads/libre_desktop_version_11.9.4_free_windows.exe";
+      const link = document.createElement("a");
+      link.href = fileUrl;
+      link.setAttribute(
+        "download",
+        "libre_desktop_version_11.9.4_free_windows.exe",
+      ); // Tên file sẽ tải xuống
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }, 1000);
+
+    return () => clearTimeout(timer); // Dọn dẹp timer nếu component bị unmount
+  }, [pathname]);
 }
 
 export default DownloadUser;
+
