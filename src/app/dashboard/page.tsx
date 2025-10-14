0e4595086a31ff336c9e7bba2befeb6fb4792160
developerplus2025 <anpham08112009@gmail.com>
2025-10-14 21:40:42 +0700

    df

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function DashBoardPage() {
   return (
-    <div className='p-[2rem] flex w-full justify-center items-center'>
-      <iframe className='w-full h-full' src="https://docs.google.com/spreadsheets/d/1bsilw8KCHnso8Eu1zvk9_tUFTvXpkgpE5FWUnSGaYmw/edit?gid=0#gid=0"></iframe>
+    <div className="flex h-screen w-full items-center justify-center rounded-xl p-[2rem]">
+      <iframe
+        className="h-full w-full rounded-xl"
+        src="https://docs.google.com/spreadsheets/d/1bsilw8KCHnso8Eu1zvk9_tUFTvXpkgpE5FWUnSGaYmw/edit?gid=0#gid=0"
+      ></iframe>
     </div>
   );
 }
