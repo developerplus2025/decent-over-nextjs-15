c1d43abe3a34998ce80a8b5a28b1326c7d1e68db
developerplus2025 <anpham08112009@gmail.com>
2025-10-15 11:05:55 +0700

    df

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -68,7 +68,7 @@ function CommandInput({
   return (
     <div
       data-slot="command-input-wrapper"
-      className="border-input flex h-9 items-center gap-2 border-x border-t border-b px-3"
+      className="border-input flex h-9 items-center gap-2 rounded-md border-x border-t border-b px-3"
     >
       <SearchIcon className="size-4 shrink-0 opacity-50" />
       <CommandPrimitive.Input
