40c04605e339b022c127cf5a71c0d2dc3f087deb
developerplus2025 <anpham08112009@gmail.com>
2025-10-19 14:21:30 +0700

    df

--- src/components/UserButtonClient.tsx
+++ src/components/UserButtonClient.tsx
@@ -40,6 +40,102 @@ function removeVietnameseTones(str?: string): string {
     .replace(/Đ/g, "D");
 }
 
+const itemsPartOne = [
+  {
+    name: "Account Setting",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <title>Account Settings</title>
+        <path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Zm88-29.84q.06-2.16,0-4.32l14.92-18.64a8,8,0,0,0,1.48-7.06,107.21,107.21,0,0,0-10.88-26.25,8,8,0,0,0-6-3.93l-23.72-2.64q-1.48-1.56-3-3L186,40.54a8,8,0,0,0-3.94-6,107.71,107.71,0,0,0-26.25-10.87,8,8,0,0,0-7.06,1.49L130.16,40Q128,40,125.84,40L107.2,25.11a8,8,0,0,0-7.06-1.48A107.6,107.6,0,0,0,73.89,34.51a8,8,0,0,0-3.93,6L67.32,64.27q-1.56,1.49-3,3L40.54,70a8,8,0,0,0-6,3.94,107.71,107.71,0,0,0-10.87,26.25,8,8,0,0,0,1.49,7.06L40,125.84Q40,128,40,130.16L25.11,148.8a8,8,0,0,0-1.48,7.06,107.21,107.21,0,0,0,10.88,26.25,8,8,0,0,0,6,3.93l23.72,2.64q1.49,1.56,3,3L70,215.46a8,8,0,0,0,3.94,6,107.71,107.71,0,0,0,26.25,10.87,8,8,0,0,0,7.06-1.49L125.84,216q2.16.06,4.32,0l18.64,14.92a8,8,0,0,0,7.06,1.48,107.21,107.21,0,0,0,26.25-10.88,8,8,0,0,0,3.93-6l2.64-23.72q1.56-1.48,3-3L215.46,186a8,8,0,0,0,6-3.94,107.71,107.71,0,0,0,10.87-26.25,8,8,0,0,0-1.49-7.06Zm-16.1-6.5a73.93,73.93,0,0,1,0,8.68,8,8,0,0,0,1.74,5.48l14.19,17.73a91.57,91.57,0,0,1-6.23,15L187,173.11a8,8,0,0,0-5.1,2.64,74.11,74.11,0,0,1-6.14,6.14,8,8,0,0,0-2.64,5.1l-2.51,22.58a91.32,91.32,0,0,1-15,6.23l-17.74-14.19a8,8,0,0,0-5-1.75h-.48a73.93,73.93,0,0,1-8.68,0,8,8,0,0,0-5.48,1.74L100.45,215.8a91.57,91.57,0,0,1-15-6.23L82.89,187a8,8,0,0,0-2.64-5.1,74.11,74.11,0,0,1-6.14-6.14,8,8,0,0,0-5.1-2.64L46.43,170.6a91.32,91.32,0,0,1-6.23-15l14.19-17.74a8,8,0,0,0,1.74-5.48,73.93,73.93,0,0,1,0-8.68,8,8,0,0,0-1.74-5.48L40.2,100.45a91.57,91.57,0,0,1,6.23-15L69,82.89a8,8,0,0,0,5.1-2.64,74.11,74.11,0,0,1,6.14-6.14A8,8,0,0,0,82.89,69L85.4,46.43a91.32,91.32,0,0,1,15-6.23l17.74,14.19a8,8,0,0,0,5.48,1.74,73.93,73.93,0,0,1,8.68,0,8,8,0,0,0,5.48-1.74L155.55,40.2a91.57,91.57,0,0,1,15,6.23L173.11,69a8,8,0,0,0,2.64,5.1,74.11,74.11,0,0,1,6.14,6.14,8,8,0,0,0,5.1,2.64l22.58,2.51a91.32,91.32,0,0,1,6.23,15l-14.19,17.74A8,8,0,0,0,199.87,123.66Z"></path>
+      </svg>
+    ),
+    directLink: "account-setting",
+  },
+  {
+    name: "DashBoard",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <title>Dashboard</title>
+        <path d="M216,72H131.31L104,44.69A15.86,15.86,0,0,0,92.69,40H40A16,16,0,0,0,24,56V200.62A15.4,15.4,0,0,0,39.38,216H216.89A15.13,15.13,0,0,0,232,200.89V88A16,16,0,0,0,216,72ZM40,56H92.69l16,16H40ZM216,200H40V88H216Z"></path>
+      </svg>
+    ),
+    directLink: "dashboard",
+  },
+  {
+    name: "Create Projects",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H136v32a8,8,0,0,1-16,0V136H88a8,8,0,0,1,0-16h32V88a8,8,0,0,1,16,0v32h32A8,8,0,0,1,176,128Z"></path>
+      </svg>
+    ),
+    directLink: "create-projects",
+  },
+];
+const itemsPartTwo = [
+  {
+    name: "Home Page",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <path d="M219.31,108.68l-80-80a16,16,0,0,0-22.62,0l-80,80A15.87,15.87,0,0,0,32,120v96a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V160h32v56a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V120A15.87,15.87,0,0,0,219.31,108.68ZM208,208H160V152a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8v56H48V120l80-80,80,80Z"></path>
+      </svg>
+    ),
+    directLink: "",
+  },
+  {
+    name: "Pricing",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <path d="M104,216a16,16,0,1,1-16-16A16,16,0,0,1,104,216Zm88-16a16,16,0,1,0,16,16A16,16,0,0,0,192,200ZM239.71,74.14l-25.64,92.28A24.06,24.06,0,0,1,191,184H92.16A24.06,24.06,0,0,1,69,166.42L33.92,40H16a8,8,0,0,1,0-16H40a8,8,0,0,1,7.71,5.86L57.19,64H232a8,8,0,0,1,7.71,10.14ZM221.47,80H61.64l22.81,82.14A8,8,0,0,0,92.16,168H191a8,8,0,0,0,7.71-5.86Z"></path>
+      </svg>
+    ),
+    directLink: "pricing",
+  },
+  {
+    name: "Documention",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <path d="M232,48H160a40,40,0,0,0-32,16A40,40,0,0,0,96,48H24a8,8,0,0,0-8,8V200a8,8,0,0,0,8,8H96a24,24,0,0,1,24,24,8,8,0,0,0,16,0,24,24,0,0,1,24-24h72a8,8,0,0,0,8-8V56A8,8,0,0,0,232,48ZM96,192H32V64H96a24,24,0,0,1,24,24V200A39.81,39.81,0,0,0,96,192Zm128,0H160a39.81,39.81,0,0,0-24,8V88a24,24,0,0,1,24-24h64Z"></path>
+      </svg>
+    ),
+    directLink: "docs",
+  },
+];
 export default function UserButtonClient() {
   const router = useRouter();
 
@@ -223,106 +319,48 @@ export default function UserButtonClient() {
                     </div>
                     <div className="border-b border-b-[#302f2f]"></div>
                     <div className="flex w-full flex-col gap-2 px-4 py-2 [&_svg]:size-4">
-                      <Button
-                        onMouseEnter={() => {
-                          setIsHovered(true); // Khi bắt đầu hover
-                          console.log("Hovered");
-                        }}
-                        onMouseLeave={() => {
-                          setIsHovered(false); // Khi kết thúc hover
-                          console.log("Unhovered");
-                        }}
-                        className={`w-full ${isHovered ? "bg-primary/90" : ""} justify-between`}
-                        variant="outline"
-                      >
-                        Account Settings
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="#ffffff"
-                          viewBox="0 0 256 256"
+                      {itemsPartTwo.map((itemsPartOne) => (
+                        <Button
+                          key={itemsPartOne.name}
+                          onMouseEnter={() => {
+                            setIsHovered(true); // Khi bắt đầu hover
+                            console.log("Hovered");
+                          }}
+                          onMouseLeave={() => {
+                            setIsHovered(false); // Khi kết thúc hover
+                            console.log("Unhovered");
+                          }}
+                          className={`w-full ${isHovered ? "bg-primary/90" : "bg-black"} justify-between`}
+                          variant="outline"
+                          onClick={handleLogout}
                         >
-                          <title>Account Settings</title>
-                          <path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160Zm88-29.84q.06-2.16,0-4.32l14.92-18.64a8,8,0,0,0,1.48-7.06,107.21,107.21,0,0,0-10.88-26.25,8,8,0,0,0-6-3.93l-23.72-2.64q-1.48-1.56-3-3L186,40.54a8,8,0,0,0-3.94-6,107.71,107.71,0,0,0-26.25-10.87,8,8,0,0,0-7.06,1.49L130.16,40Q128,40,125.84,40L107.2,25.11a8,8,0,0,0-7.06-1.48A107.6,107.6,0,0,0,73.89,34.51a8,8,0,0,0-3.93,6L67.32,64.27q-1.56,1.49-3,3L40.54,70a8,8,0,0,0-6,3.94,107.71,107.71,0,0,0-10.87,26.25,8,8,0,0,0,1.49,7.06L40,125.84Q40,128,40,130.16L25.11,148.8a8,8,0,0,0-1.48,7.06,107.21,107.21,0,0,0,10.88,26.25,8,8,0,0,0,6,3.93l23.72,2.64q1.49,1.56,3,3L70,215.46a8,8,0,0,0,3.94,6,107.71,107.71,0,0,0,26.25,10.87,8,8,0,0,0,7.06-1.49L125.84,216q2.16.06,4.32,0l18.64,14.92a8,8,0,0,0,7.06,1.48,107.21,107.21,0,0,0,26.25-10.88,8,8,0,0,0,3.93-6l2.64-23.72q1.56-1.48,3-3L215.46,186a8,8,0,0,0,6-3.94,107.71,107.71,0,0,0,10.87-26.25,8,8,0,0,0-1.49-7.06Zm-16.1-6.5a73.93,73.93,0,0,1,0,8.68,8,8,0,0,0,1.74,5.48l14.19,17.73a91.57,91.57,0,0,1-6.23,15L187,173.11a8,8,0,0,0-5.1,2.64,74.11,74.11,0,0,1-6.14,6.14,8,8,0,0,0-2.64,5.1l-2.51,22.58a91.32,91.32,0,0,1-15,6.23l-17.74-14.19a8,8,0,0,0-5-1.75h-.48a73.93,73.93,0,0,1-8.68,0,8,8,0,0,0-5.48,1.74L100.45,215.8a91.57,91.57,0,0,1-15-6.23L82.89,187a8,8,0,0,0-2.64-5.1,74.11,74.11,0,0,1-6.14-6.14,8,8,0,0,0-5.1-2.64L46.43,170.6a91.32,91.32,0,0,1-6.23-15l14.19-17.74a8,8,0,0,0,1.74-5.48,73.93,73.93,0,0,1,0-8.68,8,8,0,0,0-1.74-5.48L40.2,100.45a91.57,91.57,0,0,1,6.23-15L69,82.89a8,8,0,0,0,5.1-2.64,74.11,74.11,0,0,1,6.14-6.14A8,8,0,0,0,82.89,69L85.4,46.43a91.32,91.32,0,0,1,15-6.23l17.74,14.19a8,8,0,0,0,5.48,1.74,73.93,73.93,0,0,1,8.68,0,8,8,0,0,0,5.48-1.74L155.55,40.2a91.57,91.57,0,0,1,15,6.23L173.11,69a8,8,0,0,0,2.64,5.1,74.11,74.11,0,0,1,6.14,6.14,8,8,0,0,0,5.1,2.64l22.58,2.51a91.32,91.32,0,0,1,6.23,15l-14.19,17.74A8,8,0,0,0,199.87,123.66Z"></path>
-                        </svg>
-                      </Button>
-                      <Button
-                        onMouseEnter={() => {
-                          setIsHovered(true); // Khi bắt đầu hover
-                          console.log("Hovered");
-                        }}
-                        onMouseLeave={() => {
-                          setIsHovered(false); // Khi kết thúc hover
-                          console.log("Unhovered");
-                        }}
-                        className={`w-full ${isHovered ? "bg-primary/90" : ""} justify-between`}
-                        onClick={() => router.push("/dashboard")}
-                        variant="outline"
-                      >
-                        DashBoard
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="#ffffff"
-                          viewBox="0 0 256 256"
-                        >
-                          <title>Dashboard</title>
-                          <path d="M216,72H131.31L104,44.69A15.86,15.86,0,0,0,92.69,40H40A16,16,0,0,0,24,56V200.62A15.4,15.4,0,0,0,39.38,216H216.89A15.13,15.13,0,0,0,232,200.89V88A16,16,0,0,0,216,72ZM40,56H92.69l16,16H40ZM216,200H40V88H216Z"></path>
-                        </svg>
-                      </Button>
-                      <Button
-                        onMouseEnter={() => {
-                          setIsHovered(true); // Khi bắt đầu hover
-                          console.log("Hovered");
-                        }}
-                        onMouseLeave={() => {
-                          setIsHovered(false); // Khi kết thúc hover
-                          console.log("Unhovered");
-                        }}
-                        className={`w-full ${isHovered ? "bg-primary/90" : ""} justify-between`}
-                        onClick={() => router.push("/dashboard")}
-                        variant="outline"
-                      >
-                        Create Project
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="#ffffff"
-                          viewBox="0 0 256 256"
-                        >
-                          <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a8,8,0,0,1-8,8H136v32a8,8,0,0,1-16,0V136H88a8,8,0,0,1,0-16h32V88a8,8,0,0,1,16,0v32h32A8,8,0,0,1,176,128Z"></path>
-                        </svg>
-                      </Button>
+                          {itemsPartOne.name}
+                          {itemsPartOne.icon}
+                        </Button>
+                      ))}
                     </div>
                     <div className="border-b border-b-[#302f2f]"></div>
                     <div className="flex flex-col gap-2 px-4 py-2">
-                      <Button
-                        onMouseEnter={() => {
-                          setIsHovered(true); // Khi bắt đầu hover
-                          console.log("Hovered");
-                        }}
-                        onMouseLeave={() => {
-                          setIsHovered(false); // Khi kết thúc hover
-                          console.log("Unhovered");
-                        }}
-                        className={`w-full ${isHovered ? "bg-primary/90" : ""} justify-between`}
-                        variant="outline"
-                        onClick={handleLogout}
-                      >
-                        Home Page
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="#ffffff"
-                          viewBox="0 0 256 256"
+                      {itemsPartTwo.map((itemsPartTwo) => (
+                        <Button
+                          key={itemsPartTwo.name}
+                          onMouseEnter={() => {
+                            setIsHovered(true); // Khi bắt đầu hover
+                            console.log("Hovered");
+                          }}
+                          onMouseLeave={() => {
+                            setIsHovered(false); // Khi kết thúc hover
+                            console.log("Unhovered");
+                          }}
+                          className={`w-full ${isHovered ? "bg-primary/90" : "bg-black"} justify-between`}
+                          variant="outline"
+                          onClick={handleLogout}
                         >
-                          <path d="M219.31,108.68l-80-80a16,16,0,0,0-22.62,0l-80,80A15.87,15.87,0,0,0,32,120v96a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V160h32v56a8,8,0,0,0,8,8h64a8,8,0,0,0,8-8V120A15.87,15.87,0,0,0,219.31,108.68ZM208,208H160V152a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8v56H48V120l80-80,80,80Z"></path>
-                        </svg>
-                      </Button>
+                          {itemsPartTwo.name}
+                          {itemsPartTwo.icon}
+                        </Button>
+                      ))}
+
                       <Button
                         onMouseEnter={() => {
                           setIsHovered(true); // Khi bắt đầu hover
@@ -332,7 +370,7 @@ export default function UserButtonClient() {
                           setIsHovered(false); // Khi kết thúc hover
                           console.log("Unhovered");
                         }}
-                        className={`w-full ${isHovered ? "bg-primary/90" : ""} justify-between`}
+                        className={`w-full ${isHovered ? "bg-primary/90" : "bg-black"} justify-between`}
                         variant="outline"
                         onClick={handleLogout}
                       >
